9b98d88557a2f65336072f18e0c5c9179422c49a
SHAtwofiddy6 <sul@yman.uk>
2025-03-25 08:32:55 +0300

    Update grouping.py

--- video_pipeline/app/utils/grouping.py
+++ video_pipeline/app/utils/grouping.py
@@ -1,3 +1,13 @@
-# grouping.py
+def associate_faces_with_vehicles(faces, plates, vehicles):
+    # Naive implementation: find faces within or near vehicle bounding boxes
+    groups = []
+    for vehicle in vehicles:
+        vx1, vy1, vx2, vy2 = vehicle['bbox']
+        group = {'vehicle': vehicle, 'faces': [], 'plates': plates}
 
-# Placeholder for grouping.py
+        for face in faces:
+            fx1, fy1, fx2, fy2 = face['bbox']
+            if vx1 - 50 < fx1 < vx2 + 50 and vy1 - 50 < fy1 < vy2 + 50:
+                group['faces'].append(face)
+        groups.append(group)
+    return groups
